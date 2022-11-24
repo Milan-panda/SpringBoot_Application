10fd421acbb81310778319e79804dcc810052e96
Milan <milanpanda4425@gmail.com>
2022-11-24 17:22:24 +0530

    first commit

---
+++ src/main/java/com/mycompany/user/User.java
@@ -0,0 +1,98 @@
+package com.mycompany.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(length = 15, nullable = false)
+    private String password;
+
+    @Column(length = 45, nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(length = 45, nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(length = 10, nullable = false, name = "mobile_number")
+        private String mobileNumber;
+
+    @Column(length = 100, nullable = false, name = "address")
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
